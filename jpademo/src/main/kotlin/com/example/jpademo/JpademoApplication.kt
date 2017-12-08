fbb1046196ab6415ba3c2fede3b98b8a7970c468
Yang BaoSheng <675885711@qq.com>
2017-12-08 08:57:39 +0800

    Revert "no message"
    This reverts commit 79c2f44b3b339372efcdb2878c21d1cf5f077ea2.

--- jpademo/src/main/kotlin/com/example/jpademo/JpademoApplication.kt
+++ jpademo/src/main/kotlin/com/example/jpademo/JpademoApplication.kt
@@ -1,22 +1,10 @@
-//参照 http://www.ityouknow.com/springboot/2016/02/03/springboot(%E4%BA%8C)-web%E7%BB%BC%E5%90%88%E5%BC%80%E5%8F%91.html
-
 package com.example.jpademo
 
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-@RestController
-class JpademoApplication{
-
-    @RequestMapping("/")
-    fun hello():String{
-        return "Hello"
-    }
-}
+class JpademoApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(JpademoApplication::class.java, *args)
--- jpademo/src/main/kotlin/com/example/jpademo/UserRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jpademo
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByUserName (userName:String):User
-
-    fun findByUserNameOrEmail (username:String, email:String ):User
-}
